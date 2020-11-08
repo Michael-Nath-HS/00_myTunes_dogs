e2fad09931d566589a1a921a66086551fb743e48
Victor <siuleungjing@gmail.com>
2020-11-07 20:17:06 -0500

    Remove extra insert_order function

--- songs.c
+++ songs.c
@@ -29,7 +29,6 @@ struct song_node *insert_front(struct song_node *p, char *artist, char *title) {
 
 struct song_node *insert_order(struct song_node *p, char *artist, char *title) {
     struct song_node *new = new_node(artist, title);
-
     if (p == NULL) {
         return new;
     }
@@ -45,51 +44,6 @@ struct song_node *insert_order(struct song_node *p, char *artist, char *title) {
     p -> next = insert_order(p -> next, artist, title);
     return p;
 }
-
-// insert nodes in order alphabetical by Artist then by Song
-/*
-struct song_node *insert_order(struct song_node *p, char *artist, char *title) {
-    struct song_node *new_one = new_node(artist, title);
-    if (p == NULL) {
-        return new_one;
-    }
-
-    struct song_node *previous = NULL;
-    struct song_node *current = p;
-    while(current){
-        int comparison = strcmp(artist, current->artist);
-
-        if(comparison < 0) {
-            //Insert new_one inbetween previous and current
-            if(previous == NULL) return insert_front(current, artist, title);
-            new_one->next = previous->next;
-            previous->next = new_one;
-            return p;
-        }
-        if(comparison == 0){
-            //Note: Two titles will never be equal
-            //If smaller, put new node into list
-            if (strcmp(title, current->title) < 0) {
-                if(previous == NULL) return insert_front(current, artist, title);
-                //Insert new_one inbetween previous and current
-                new_one->next = previous->next;
-                previous->next = new_one;
-                return p;
-            }
-            //If bigger, let the loop run to the next node
-        }
-        if(current->next == NULL) {
-            current->next = new_one;
-            return p;
-        }
-        //Increment to next
-        previous = current;
-        current = current->next;
-    }
-    printf("This should never display\n");
-}
-*/
-
 
 // print the entire list
 void print_list(struct song_node *p) {
@@ -136,7 +90,6 @@ struct song_node *find_first_song(struct song_node *p, char *artist) {
 
 // Return a pointer to random element in the list.
 struct song_node *random_song(struct song_node *p) {
-
     struct song_node *res = p;
     int n = 1;
     for (n = 2; p != NULL; n++) {
